c1b9393fbbfaf4911b04f73f2ab78332ebea29a6
pondus72 <pondus72@bildesiden.com>
2026-07-04 22:37:21 +0200

    Add CakeDivider package exports

---
+++ cake_divider/__init__.py
@@ -0,0 +1,5 @@
+"""Parametric CadQuery model for a printable cake divider."""
+
+from .model import DividerParameters, build_models, export_models
+
+__all__ = ["DividerParameters", "build_models", "export_models"]
